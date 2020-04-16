a80b90c946e77251819624424a5fbbdf0a0ac6eb
Heejin Park <p14jeffwest@gmail.com>
2020-04-16 10:21:28 +0900

    jeff\coord.py

--- jeff/coord.py
+++ jeff/coord.py
@@ -237,3 +237,27 @@ class CoordinateEx1(Scene):
         rect2.next_to(rect1, DOWN, buff=0)
         return VGroup(rect1, rect2)
 
+class CoordinateEx2(Scene):
+    def construct(self):
+        dot = Dot()
+
+        dot.to_edge(LEFT)
+        self.add(dot)
+        self.wait()
+
+        for i in range(0,5):
+            self.move_dot(dot)
+
+
+    def move_dot(self, dot):
+        d1 = dot.copy()
+        d2 = dot.copy()
+        d3 = dot.copy()
+
+        self.play(
+            d1.to_corner, UR,
+            d2.to_edge, RIGHT,
+            d3.to_corner, DR,
+        )
+        self.remove(d1, d2, d3)
+
